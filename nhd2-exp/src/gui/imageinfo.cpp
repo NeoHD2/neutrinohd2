c75627b62de9f49cda1f515de870865dba4aee46
mohousch <mohousch@gmail.com>
2021-08-24 16:50:52 +0200

    - increase version.

--- nhd2-exp/src/gui/imageinfo.cpp
+++ nhd2-exp/src/gui/imageinfo.cpp
@@ -171,7 +171,7 @@ void CImageInfo::paint()
 	const char * gitrev = GIT;
 	
 	//static CFlashVersionInfo versionInfo(version);
-	const char * releaseCycle = "3.7";
+	const char * releaseCycle = "3.8";
 	//releaseCycle = versionInfo.getReleaseCycle();
 	const char * imageType = "Snapshot";
 	//imageType = versionInfo.getType();
